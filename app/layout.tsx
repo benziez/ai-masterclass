ae8406fdbc1e1e1f08e5a825100a47f7eae2236d
benziez <thebenjerlecki@gmail.com>
2025-10-05 21:14:12 -0400

    Simplify Facebook pixel implementation - revert to working method with simple Lead event

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
 import ClientExitIntentWrapper from './providers/ClientExitIntentWrapper'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
-import FacebookPixel from '@/components/FacebookPixel'
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -81,10 +80,33 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        {/* Facebook Pixel will be loaded by component */}
+        {/* Facebook Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2455252148193058');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2455252148193058&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={`${inter.className} antialiased`}>
-        <FacebookPixel />
         <GoogleAnalytics />
         <ClientExitIntentWrapper>
           {children}
